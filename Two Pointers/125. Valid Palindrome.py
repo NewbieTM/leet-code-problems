ce0846b2d796e14d94fb8dfa4fefab7792a01ee3
Timur Moldovskiy <94910707+NewbieTM@users.noreply.github.com>
2024-11-05 20:18:55 +0200

    Update 125. Valid Palindrome.py

--- Two Pointers/125. Valid Palindrome.py
+++ Two Pointers/125. Valid Palindrome.py
@@ -1,1 +1,18 @@
-print(1)
+#https://leetcode.com/problems/valid-palindrome/
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            while not s[l].isalnum() and l < r:
+                l+=1
+            while not s[r].isalnum() and r > l:
+                r-=1
+        
+            if s[l] != s[r]:
+                return (False)
+            l+=1
+            r-=1
+            
+        return(True)
+            
