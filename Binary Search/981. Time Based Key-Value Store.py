ad82b57e5111d0161d0d7a949a057c885d7a7cfd
Timur Moldovskiy <94910707+NewbieTM@users.noreply.github.com>
2024-12-29 12:44:40 +0200

    Update 981. Time Based Key-Value Store.py

--- Binary Search/981. Time Based Key-Value Store.py
+++ Binary Search/981. Time Based Key-Value Store.py
@@ -31,3 +31,28 @@ class TimeMap:
             return ''
         
 
+# OR (LESS MEMORY USAGE)
+
+class TimeMap:
+
+    def __init__(self):
+        self.sorted_times = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.sorted_times[key].append([value,timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ''
+        v_and_t_per_key = self.sorted_times.get(key,[])
+        
+        l,r = 0, len(v_and_t_per_key) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if v_and_t_per_key[mid][1] <= timestamp:
+                res = v_and_t_per_key[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
+
+
